e5ea7e670887fa15413df29ef3690b7ad7df9650
Marina Rappoport <rappoportmarina@gmail.com>
2020-09-26 22:42:07 +0300

    renamed FileDetails to Book

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -1,13 +1,22 @@
 package model;
 
 import java.util.Date;
+import java.util.LinkedList;
 
 public class Book {
 	private long id;
-	private String title;
-	private String author;
+	private String title, date;
+	private String author, path;
 	private String translator;
 	private Date releaseDate;
+	public int countWord, lineCount, sentenceCount, paragraphCount, characterCount;
+
+	public Book(String title, String author, String date, String path) {
+		this.title = title;
+		this.author = author;
+		this.date = date; //TODO string date to Date
+		this.path = path;
+	}
 
 	public Book(String title, String author, String translator, Date releaseDate) {
 		this.title = title;
@@ -16,16 +25,32 @@ public class Book {
 		this.releaseDate = releaseDate;
 	}
 
+	public Book(String path) {
+		this.path = path;
+	}
+
 	public long getId() {
 		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
 	}
 
 	public String getTitle() {
 		return title;
 	}
 
+	public String getDate() {
+		return date;
+	}
+
 	public String getAuthor() {
 		return author;
+	}
+
+	public String getPath() {
+		return path;
 	}
 
 	public String getTranslator() {
@@ -34,9 +59,5 @@ public class Book {
 
 	public Date getReleaseDate() {
 		return releaseDate;
-	}
-
-	public void setId(long id) {
-		this.id = id;
 	}
 }
