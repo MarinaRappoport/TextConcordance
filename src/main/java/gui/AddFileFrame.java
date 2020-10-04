29b997813f1992565513f8b569aa0937512920ce
Gitit Kurbet <gitit.c@gmail.com>
2020-10-04 21:54:15 +0300

    update AddFileFrame : 1) Press enter to save changes 2) Change arrayList<Book> books to Map<Book, List<WordLocation> > bookMap

--- src/main/java/gui/AddFileFrame.java
+++ src/main/java/gui/AddFileFrame.java
@@ -17,16 +17,12 @@ import java.util.List;
 
 //GUI of adding file window
 public class AddFileFrame extends JFrame {
-    JButton addFile, ok, prev, next;
-    int currentBookIndex;
-    JLabel title, releaseDate, author, translator, chooseFile, currPage;
-    JPanel mainPanel, pagesPanel, bottomPanel, okPanel;
-    JTextField dateTF, authorTF, titleTF, translatorTF;
-	//TODO need to add listener in order to update values when user changed it
-    FilesManager filesManager;
-    String fileName, filePath;
-    ArrayList <Book> books;
-    //TODO replace arrayList with map or add WordLocation to Book object
+    private JButton addFile, ok, prev, next;
+    private int currBook;
+    private JLabel title, releaseDate, author, translator, chooseFile, currPage, note;
+    private JPanel mainPanel, pagesPanel, bottomPanel, okPanel;
+    private JTextField dateTF, authorTF, titleTF, translatorTF;
+    private FilesManager filesManager;
 	private Map<Book, List<WordLocation>> bookMap;
 
     //final Font TITLE_FONT = new Font("Title", Font.TRUETYPE_FONT,22);
@@ -34,17 +30,16 @@ public class AddFileFrame extends JFrame {
     final Color PRIMARY_COLOR = new Color(18, 163, 134, 99);
     final Color SECONDARY_COLOR = new Color(18, 163, 134, 190);
     final Color DEFAULT = new Color(206, 200, 200, 2);
-    final Border BORDER = BorderFactory.createLineBorder(DEFAULT, 40);
+    final Border BORDER = BorderFactory.createLineBorder(DEFAULT, 35);
 
     public AddFileFrame(){
-        books = new ArrayList<>();
 	    bookMap = new HashMap<>();
         filesManager = FilesManager.getInstance();
-        currentBookIndex = 0;
+        currBook = 0;
 
         mainPanel = new JPanel();
         mainPanel.setBorder(BORDER);
-        mainPanel.setLayout(new GridLayout(5, 2, 20, 20));
+        mainPanel.setLayout(new GridLayout(6, 2, 20, 20));
 
         bottomPanel = new JPanel();
         bottomPanel.setBorder(BORDER);
@@ -69,6 +64,7 @@ public class AddFileFrame extends JFrame {
         releaseDate.setFont(MY_FONT);
         chooseFile = new JLabel("Choose files");
         chooseFile.setFont(MY_FONT);
+        note = new JLabel("*Note : Press enter to save changes");
 
         //Choose file with JFileChooser
         addFile = new JButton("Select files");
@@ -87,37 +83,57 @@ public class AddFileFrame extends JFrame {
                     Arrays.asList(files).forEach(x -> {
 	                    Book newBook = new Book(x.getAbsolutePath());
 	                    List<WordLocation> list = new FileParser().parseFile(newBook);
-                        books.add(newBook);
 	                    bookMap.put(newBook, list);
                     });
 
                     updateBookDetails();
 
-                    //addFile.setText(selectedFile.getName());
-                    //filePath = selectedFile.getAbsolutePath();
-                    //fileName = selectedFile.getName();
                 }
                 }
 
         });
 
+        ActionListener textFieldListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Object[] books = bookMap.keySet().toArray();
+
+                if ( e.getSource() == titleTF ){
+                    ((Book)books[currBook -1]).setTitle(titleTF.getText());
+                } else if ( e.getSource() == translatorTF ){
+                    ((Book)books[currBook -1]).setTranslator(translatorTF.getText());
+                } else if ( e.getSource() == dateTF ) {
+                    ((Book)books[currBook -1]).setDate(dateTF.getText());
+                } else if ( e.getSource() == authorTF ){
+                    ((Book)books[currBook -1]).setAuthor(authorTF.getText());
+                }
+            }
+        };
+
         titleTF = new JTextField();
         translatorTF = new JTextField();
-        dateTF = new JTextField("--/--/--");
+        dateTF = new JTextField();
         authorTF = new JTextField();
+
+        titleTF.addActionListener(textFieldListener);
+        translatorTF.addActionListener(textFieldListener);
+        dateTF.addActionListener(textFieldListener);
+        authorTF.addActionListener(textFieldListener);
+
         //TODO : check date format
 
         //Create labels and buttons on the bottom panel :
 
-        ActionListener listener = new ActionListener() {
+        ActionListener btnListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if ( e.getSource() == prev ) {
-                    if ( currentBookIndex > 0 )
-                        currentBookIndex -= 1;
+                    if ( currBook > 0 )
+                        currBook -= 1;
                 } else {
-                    if ( currentBookIndex < books.size() )
-                        currentBookIndex += 1;
+                    if ( currBook < bookMap.size() )
+                        currBook += 1;
                 }
 
                 updateBookDetails();
@@ -125,21 +141,20 @@ public class AddFileFrame extends JFrame {
         };
 
         prev = new JButton("previous");
-        prev.addActionListener(listener);
+        prev.addActionListener(btnListener);
         next = new JButton("next");
-        next.addActionListener(listener);
-        currPage = new JLabel(currentBookIndex + "/0");
+        next.addActionListener(btnListener);
+        currPage = new JLabel(currBook + "/0");
 
 
         ok = new JButton("OK");
         ok.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Iterator<Book> iter = books.iterator();
-                while (iter.hasNext()) {
-                	//TODO alert for waiting and for DONE
-                	Book book = iter.next();
-                    filesManager.addFile(book, bookMap.get(book));
+
+                for (Map.Entry<Book, List<WordLocation>> entry : bookMap.entrySet()){
+                    //TODO alert for waiting and for DONE
+                    filesManager.addFile(entry.getKey(), entry.getValue());
                 }
             }
 
@@ -156,6 +171,7 @@ public class AddFileFrame extends JFrame {
         mainPanel.add(translatorTF);
         mainPanel.add(releaseDate);
         mainPanel.add(dateTF);
+        mainPanel.add(note);
 
         okPanel.add(ok);
 
@@ -167,7 +183,6 @@ public class AddFileFrame extends JFrame {
         bottomPanel.add(pagesPanel, BorderLayout.CENTER);
         bottomPanel.add(okPanel, BorderLayout.SOUTH);
 
-        //add(title, BorderLayout.NORTH);
         add(mainPanel, BorderLayout.CENTER);
         add(bottomPanel, BorderLayout.SOUTH);
 
@@ -175,13 +190,15 @@ public class AddFileFrame extends JFrame {
     }
 
     private void updateBookDetails() {
-        if ( ( currentBookIndex == 0 ) && !books.isEmpty() )
-            currentBookIndex = 1;
+        if ( ( currBook == 0 ) && !bookMap.isEmpty() )
+            currBook = 1;
 
-        currPage.setText(currentBookIndex + "/" + books.size() );
-        titleTF.setText(books.get(currentBookIndex-1).getTitle());
-	    authorTF.setText(books.get(currentBookIndex-1).getAuthor());
-	    translatorTF.setText(books.get(currentBookIndex-1).getTranslator());
-		dateTF.setText(books.get(currentBookIndex-1).getDate());
+        Object[] books = bookMap.keySet().toArray();
+
+        currPage.setText(currBook + "/" + bookMap.size() );
+        titleTF.setText(((Book)books[currBook -1]).getTitle());
+	    authorTF.setText(((Book)books[currBook -1]).getAuthor());
+	    translatorTF.setText(((Book)books[currBook -1]).getTranslator());
+		dateTF.setText(((Book)books[currBook -1]).getDate());
     }
 }
